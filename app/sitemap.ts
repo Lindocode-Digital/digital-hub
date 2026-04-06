009ca13b48e04831fc2f7479bdc198fbc69a7243
Lindo-code <sdrowvieli1@gmail.com>
2026-04-06 17:55:45 +0200

    seo updates

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://lindocode.com",
+      priority: 1,
+    },
+    {
+      url: "https://lindocode.com/lazyappz",
+      priority: 0.9,
+    },
+    {
+      url: "https://lindocode.com/projects",
+      priority: 0.8,
+    },
+  ];
+}
